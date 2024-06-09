318def1ffc7b581409d52c7c62f79e459fbada12
Akshat-iitk <akshatt21@iitk.ac.in>
2024-06-09 17:59:25 +0530

    Added a login component using appwrite login as a service

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
 
 function Button({
-    children,
-    type = "button",
-    bgColor = "bg-blue-600",
-    textColor = "text-white",
-    className = "",
-    ...props // spread other property if provided
+  children,
+  type = "button",
+  bgColor = "bg-blue-600",
+  textColor = "text-white",
+  className = "",
+  ...props // spread other property if provided
 }) {
-    return (
-        <button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`} {...props}>
-            {children}
-        </button>
-    );
+  return (
+    <button
+      className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 }
 
-export default Button
+export default Button;
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import Footer from "./footer/Footer";
 import Header from "./header/Header";
-import Container from "./container/Container"
+import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./header/LogoutBtn";
 import Button from "./Button";
-export  { Header, Footer ,Container ,Logo , LogoutBtn , Button};
+import Input from "./Input";
+export { Header, Footer, Container, Logo, LogoutBtn, Button, Input };
